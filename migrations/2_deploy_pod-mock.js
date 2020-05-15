68d54b780bb0649751654f16a0f134eb8a227317
masaun <masanoriuno@gmail.com>
2020-05-15 18:25:58 +0900

    Commentout podMock.transferOwnership() because of avoiding an error of 'not function'

--- migrations/2_deploy_pod-mock.js
+++ migrations/2_deploy_pod-mock.js
@@ -11,17 +11,21 @@ const _erc20 = tokenAddressList["Kovan"]["DAI"];     // DAI address on Kovan
 
 
 module.exports = async function(deployer, network, accounts) {
+    // Deployer address
+    let deployerAddress = accounts[0];
+
     // Initialize owner address if you want to transfer ownership of contract to some other address
     let ownerAddress = walletAddressList["WalletAddress1"];
 
     // Deploy library and then link library with contract
-    deployer.deploy(FixedPoint);
-    deployer.link(FixedPoint, PodMock);
+    await deployer.deploy(FixedPoint);
+    await deployer.link(FixedPoint, PodMock);
 
-    await deployer.deploy(PodMock, _erc20).then(async function(podMock) {
-        if(ownerAddress && ownerAddress!="") {
-            console.log(`=== Transfering ownerhip to address ${ownerAddress} ===`)
-            await podMock.transferOwnership(ownerAddress);
-        }
-    });
+    await deployer.deploy(PodMock, _erc20, { from: deployerAddress });
+    // await deployer.deploy(PodMock, _erc20).then(async function(podMock) {
+    //     if(ownerAddress && ownerAddress!="") {
+    //         console.log(`=== Transfering ownerhip to address ${ownerAddress} ===`)
+    //         await podMock.transferOwnership(ownerAddress);
+    //     }
+    // });
 };
