87cc302236ec0431677b071e5c82beafb67f37bb
masaun <masanoriuno@gmail.com>
2020-05-15 19:03:42 +0900

    Success to deploy PoolMock.sol

--- contracts/pooltogether/PoolMock.sol
+++ contracts/pooltogether/PoolMock.sol
@@ -3,4 +3,14 @@ pragma solidity ^0.5.0;
 import "../../contracts_archive/pooltogether/pooltogether-contracts/contracts/MCDAwarePool.sol";
 //import "@pooltogether/pooltogether-contracts/contracts/MCDAwarePool.sol";
 
-contract PoolMock is MCDAwarePool {}
+contract PoolMock is MCDAwarePool {
+
+    using SafeMath for uint;
+
+    MCDAwarePool public pool;
+    IERC20 public dai;
+
+    constructor(address _erc20) public {
+        dai = IERC20(_erc20);
+    }
+}
