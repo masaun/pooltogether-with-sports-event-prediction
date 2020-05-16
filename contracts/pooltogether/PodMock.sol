4d3fd024f2aba8463fa9c23587d7f15aaa15d648
masaun <masanoriuno@gmail.com>
2020-05-16 16:42:24 +0900

    Add initPoolToken() for executing init() of PoolToken.sol

--- contracts/pooltogether/PodMock.sol
+++ contracts/pooltogether/PodMock.sol
@@ -3,14 +3,10 @@ pragma solidity ^0.5.0;
 import "../../contracts_archive/pooltogether/pods/contracts/Pod.sol";
 //import "@pooltogether/pods/contracts/Pod.sol";
 
-//import "@openzeppelin/contracts-ethereum-package/token/ERC20/IERC20.sol";
-
-
 contract PodMock is Pod {
 
     using SafeMath for uint;
 
-    Pod public pod;
     IERC20 public dai;
 
     constructor(address _erc20) public {
--- contracts/pooltogether/PoolTokenMock.sol
+++ contracts/pooltogether/PoolTokenMock.sol
@@ -3,4 +3,24 @@ pragma solidity ^0.5.0;
 import "../../contracts_archive/pooltogether/pooltogether-contracts/contracts/RecipientWhitelistPoolToken.sol";
 //import "@pooltogether/pooltogether-contracts/contracts/RecipientWhitelistPoolToken.sol";
 
-contract PoolTokenMock is RecipientWhitelistPoolToken {}
+contract PoolTokenMock is RecipientWhitelistPoolToken {
+    using SafeMath for uint;
+
+    IERC20 public dai;
+
+    constructor(address _erc20) public {
+        dai = IERC20(_erc20);
+    }
+
+
+    function initPoolToken(
+        string memory _name,
+        string memory _symbol,
+        address[] memory _defaultOperators,
+        BasePool _pool
+    ) public returns (bool) {
+        init(_name, _symbol, _defaultOperators, _pool);  // init() of PoolToken.sol
+    }
+    
+
+}
