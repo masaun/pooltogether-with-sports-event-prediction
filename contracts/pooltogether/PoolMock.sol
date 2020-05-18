5aacd9de7780aa3937d44a85f9cb4f4448d7fecd
masaun <masanoriuno@gmail.com>
2020-05-18 15:09:52 +0900

    Add balanceOfContract() into PoolMock.sol

--- contracts/pooltogether/PoolMock.sol
+++ contracts/pooltogether/PoolMock.sol
@@ -21,9 +21,18 @@ contract PoolMock is MCDAwarePool {
     }
     
 
+
     /**
      * @notice - Getter functions
      */
+    function balanceOfContract() public view returns (address poolMockContractAddess, uint balanceOfContract_DAI, uint balanceOfContract_ETH) {
+        address _poolMockContractAddess = getContractAddress();
+        return (_poolMockContractAddess, dai.balanceOf(address(this)), address(this).balance);
+    }    
 
+    function getContractAddress() public returns (address poolMockContractAddess) {
+        return address(this);
+    }
+    
     
 }
