7145c6f83f7feb54886ce8e40246c996bc6347e5
masaun <masanoriuno@gmail.com>
2020-05-25 21:33:30 +0900

    Move oracle of band-protocol into PoolMock.sol. But it was happen an error of 'gas limit'

--- contracts/pooltogether/PoolMock.sol
+++ contracts/pooltogether/PoolMock.sol
@@ -6,11 +6,14 @@ import "./pooltogether-contracts/contracts/MCDAwarePool.sol";
 import "../storage/McStorage.sol";
 import "../storage/McConstants.sol";
 
+/// Inherit from `usingBandProtocol` to get access to helper functions
+import { usingBandProtocol, Oracle } from "../band/band-solidity/contracts/Band.sol";
+
 /// Own contract
 import "../Prediction.sol";
 
 
-contract PoolMock is MCDAwarePool, McStorage, McConstants {  /// MCDAwarePool inherits BasePool.sol 
+contract PoolMock is usingBandProtocol, MCDAwarePool, McStorage, McConstants {  /// MCDAwarePool inherits BasePool.sol 
     using SafeMath for uint;
 
     IERC20 public dai;
@@ -51,17 +54,18 @@ contract PoolMock is MCDAwarePool, McStorage, McConstants {  /// MCDAwarePool in
      **/
     function _reward(bytes32 _secret, bytes32 _salt) public payable {
         /// Transfer 0.001 ETH into Prediction.sol
-        uint amount = msg.value;
+        //uint amount = msg.value;
         //uint gasLimit = 20317;
         //PREDICTION.call.value(amount).gas(gasLimit)(); 
-        PREDICTION.transfer(amount); 
+        //PREDICTION.transfer(amount); 
 
         /// Get result and identify winners and distribute reward which is inherited from Prediction.sol
         uint8 gameScore1;
         uint8 gameScore2;
         address _poolMock = address(this);
         uint _drawId = getCurrentCommittedDrawId();
-        (gameScore1, gameScore2) = prediction.getResultOfGameScore(_poolMock, _drawId, _secret, _salt);
+        (gameScore1, gameScore2) = getResultOfGameScore(_poolMock, _drawId, _secret, _salt);
+        //(gameScore1, gameScore2) = prediction.getResultOfGameScore(_poolMock, _drawId, _secret, _salt);
 
         /// Lock tokens
         lockTokens();
@@ -70,6 +74,60 @@ contract PoolMock is MCDAwarePool, McStorage, McConstants {  /// MCDAwarePool in
         reward(_secret, _salt);        
     }
 
+    /***
+     * @notice - Get result and identify winners and distribute reward 
+     **/
+    function getResultOfGameScore(
+        address _poolMock, 
+        uint _drawId, 
+        bytes32 _secret, 
+        bytes32 _salt
+    ) public payable returns (uint8 _gameScore1, uint8 _gameScore2) {
+        /// Call result of game score via Oracle
+        uint8 _gameScore1;
+        uint8 _gameScore2;
+        (_gameScore1, _gameScore2) = oracleQueryScore();
+
+        /// Count participants of specified drawId
+        uint currentCommittedDrawId = getCurrentCommittedDrawId();
+
+        /// Identify winners in all participants of specified drawId
+        for (uint i=1; i <= prediction.getCountOfPredictionData(_drawId); i++) {
+            PredictionData memory predictionData = predictionDatas[i];
+        }
+
+        return (_gameScore1, _gameScore2);
+    }
+
+    /***
+     * @notice - Oracle by using Band-Protocol
+     **/
+    function getQueryPrice() public view returns (uint256 queryPrice) {
+        /// Get the price of querying for one data point (in Wei)
+        uint256 queryPrice = SPORT.queryPrice();
+        return queryPrice;
+    }
+    
+    function oracleQuerySpotPrice() public payable {
+        /// Get the most-up-to-date ETH/USD rate
+        uint256 ethUsdPrice = FINANCIAL.querySpotPrice("ETH-USD");
+        emit OracleQuerySpotPrice(ethUsdPrice);
+    }
+
+    function oracleQuerySpotPriceWithExpiry() public payable {
+        /// Get the most-up-to-date ETH/USD rate. Must not be older than 10 mins.
+        uint256 ethUsdPrice = FINANCIAL.querySpotPriceWithExpiry("ETH-USD", 10 minutes);
+        emit OracleQuerySpotPriceWithExpiry(ethUsdPrice);
+    }    
+
+    function oracleQueryScore() public payable returns (uint8 gameScore1, uint8 gameScore2) {
+        /// 1st MLB match of the Astros vs the Tigers on August 19, 2019
+        uint8 res1;
+        uint8 res2;
+        (res1, res2) = SPORT.queryScore("MLB/20190819/HOU-DET/1");
+        emit OracleQueryScore(res1, res2);
+        return (res1, res2);
+    }
 
 
     /**
